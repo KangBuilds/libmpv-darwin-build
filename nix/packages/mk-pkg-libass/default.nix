32d9deffee7d0b535e97277d96831a3c9d647230
Chris Wei <kangplus7@gmail.com>
2026-07-17 20:25:16 +1200

    fix: isolate libass ltmain patch

--- nix/packages/mk-pkg-libass/default.nix
+++ nix/packages/mk-pkg-libass/default.nix
@@ -27,7 +27,7 @@ let
     chmod -R 777 $src
 
     cd $src
-    patch -p1 <${../../../patches/ltmain-target-passthrough.patch}
+    patch -p1 <${../../../patches/libass-ltmain-target-passthrough.patch}
     cd -
 
     cp ${./meson.build} $src/meson.build
